88651b921e4aac240fc2cf373fd2c93efdad920f
pareryy <139094361+pareryy@users.noreply.github.com>
2023-07-10 10:38:52 +0700

    Add files via upload

---
+++ Hello.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{    cout<<"Student Code : 6606021630053"<<endl;
+	cout<<"Student Name : Sasiporn Phokhow"<<endl;
+	cout<<"Division : Information Technology"<<endl;
+	cout<<"Year : 1"<<endl;
+	cout<<"Room : RA"<<endl;
+	cout<<"Sex : female"<<endl;
+	cout<<"Age : 18"<<endl;
+			 
+     system("pause");
+	 return(0);
+
+}   
