868f1eada86a892156a0181dab2d08eb199e9e8c
pareryy <139094361+pareryy@users.noreply.github.com>
2023-07-17 10:45:25 +0700

    Add files via upload

---
+++ ps2.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{   float w; 
+    float h;
+    cout << "Enter weight : " ;
+	cin >> w;
+    cout << "Enter height : " ;
+	cin >> h;
+    cout << " BMI = " << w / (( h/100)*(h/100)) << endl;
+    system("pause");
+	return(0);
+}
