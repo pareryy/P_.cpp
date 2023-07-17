2a0c648ef114d8993622236884dda8b85606c305
pareryy <139094361+pareryy@users.noreply.github.com>
2023-07-17 10:34:52 +0700

    Add files via upload

---
+++ cpp2.cpp
@@ -0,0 +1,15 @@
+#include < iostream >
+#include < string >
+using namespace std;
+int main ()
+{
+	string Name ;
+	int Year;
+    cout << "Enter name :" ;
+	cin >> Name;
+    cout << "Enter age :" ;
+	cin >> Year;
+	cout << "Your age" << 2566 - Year << "year old." <<endl;
+
+	return (0);
+}
