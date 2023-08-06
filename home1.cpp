21367be11b1257e64777d51779ba78d49869c838
pareryy <139094361+pareryy@users.noreply.github.com>
2023-08-06 22:52:27 +0700

    Add files via upload

---
+++ home1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+	char A,B,C ;
+	cout <<"Enter character(A,B,C) : ";
+	cin >> A ;
+	cout <<"Enter character(A,B,C) : ";
+	cin >> B ;
+	cout <<"Enter character(A,B,C) : ";
+	cin >> C ;
+
+	if (A=='A',B=='B',C=='C')
+	{	cout<<"A=10,B=20,C=30\n" ;
+		cout<<"10+20+30 = " << 10+20+30 << endl;
+	}
+	system("pause");
+	return(0);
+}
